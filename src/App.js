f54988c8faba927498965c04ab9f712e5ac0e85c
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2020-06-09 07:50:07 +0530

    initial frontend work finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Recipe Finder</h1>
+      <form>
+        <input
+          className="recipe-input"
+          type="text"
+          name="recipe"
+          placeholder="Enter the name of the dish"
+        />
+        <input className="btn-submit" type="submit" value="Get Ingredients" />
+      </form>
     </div>
   );
 }
