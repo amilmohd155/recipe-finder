b0cae9ce9b77f19fb2c95687b2b16519379bd9f9
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2020-06-09 08:18:50 +0530

    onclick function created and recipe information widget files created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="header">Recipe Finder</h1>
-      <form>
-        <input
-          className="recipe-input"
-          type="text"
-          name="recipe"
-          placeholder="Enter the name of the dish"
-        />
-        <input className="btn-submit" type="submit" value="Get Ingredients" />
-      </form>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", showResults: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ showResults: true });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="header">Recipe Finder</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="recipe-input"
+            type="text"
+            name="recipe"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Enter the name of the dish"
+          />
+          <input className="btn-submit" type="submit" value="Get Ingredients" />
+        </form>
+        {this.state.showResults ? (
+          <div>{this.state.value}</div>
+        ) : (
+          <h1>
+            Type a Dish Name to Search for it's Ingredients
+          </h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
