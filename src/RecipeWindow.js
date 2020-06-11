f7d07e9631fad1ba06f2b93bdc6b958d188263fe
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2020-06-11 10:19:48 +0530

    recipe title created and added material core and icon to project

--- src/RecipeWindow.js
+++ src/RecipeWindow.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./RecipeWindow.css";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 class RecipeWindow extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <p>{this.props.value}</p>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="recipe-main-div">
+          <h3>title</h3>
+          <FavoriteBorderIcon style={{ fontSize: "15px" }} />
+        </div>
+        <div>
+            
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RecipeWindow;
